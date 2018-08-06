89719dd26b2dde7c11f38ac7b326bf9335c88fc0
Muhamed Karic <muhamed.karic2017@gmail.com>
2018-08-06 15:01:36 +0200

    KONACNO, evo velika slova rijesena

--- src/ProcessAString.java
+++ src/ProcessAString.java
@@ -57,7 +57,10 @@ class ProcessAString {
 	public int countUppercaseLetters(String str) {
 
 		int count = 0;
-		
+		for(int i=0;i<str.length();i++) {
+			if(str.charAt(i)>='A' && str.charAt(i)<='Z')
+				count++;
+		}
 		
 		return count;
 	}
